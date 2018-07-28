0b23a0b03367c3e7b5653098c8a8bd48f50cb555
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-28 21:24:20 +1000

    initial data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import initialData from "./initial-data";
 
-const App = () => 'Hello world'
+class App extends React.Component {
+    state = initialData;
+
+    render() {
+        return this.state.columnOder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return column.title;
+        })
+    }
+}
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
