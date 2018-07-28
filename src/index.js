19f1ec3e7fae988b3d39144c55e6ffa0158bfcc4
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-28 22:19:13 +1000

    wrap columns into DragDropContext

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
 import initialData from "./initial-data";
 import Column from "./Column";
 
 class App extends React.Component {
   state = initialData;
 
+  onDragEnd = result => {};
   render() {
-    return this.state.columnOder.map(columnId => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(id => this.state.tasks[id]);
+    return;
+    <DragDropContext onDragEnd={this.onDragEnd}>
+      {this.state.columnOder.map(columnId => {
+        const column = this.state.columns[columnId];
+        const tasks = column.taskIds.map(id => this.state.tasks[id]);
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    });
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </DragDropContext>;
   }
 }
 
