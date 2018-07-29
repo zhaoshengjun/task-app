4a2dd2146560b13b21b35a66adf2bb4889abb0fe
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 23:43:20 +1000

    use shouldComponentUpdate to optimize re-render

--- src/Column.js
+++ src/Column.js
@@ -22,6 +22,20 @@ const TaskList = styled.div`
   min-height: 100px;
 `;
 
+class InnerList extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.tasks === this.props.tasks) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    return this.props.tasks.map((task, index) => (
+      <Task key={task.id} task={task} index={index} />
+    ));
+  }
+}
+
 export default class Column extends React.Component {
   render() {
     return (
@@ -38,9 +52,7 @@ export default class Column extends React.Component {
                   {...provided.droppableProps}
                   isDraggingOver={snapshot.isDraggingOver}
                 >
-                  {this.props.tasks.map((task, index) => (
-                    <Task key={task.id} task={task} index={index} />
-                  ))}
+                  <InnerList tasks={this.props.tasks} />
                   {provided.placeholder}
                 </TaskList>
               )}
