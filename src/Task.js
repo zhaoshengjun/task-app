5e212c65df93dba284178567566f6a3584f40ab0
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 20:13:42 +1000

    update task style based on snapshot

--- src/Task.js
+++ src/Task.js
@@ -8,17 +8,19 @@ const Container = styled.div`
   background: white;
   padding: 8px;
   margin-bottom: 8px;
+  background-color:${props => (props.isDragging ? "lightgreen" : "white")}
 `;
 
 export default class Task extends React.Component {
   render() {
     return (
       <Draggable draggableId={this.props.task.id} index={this.props.index}>
-        {provided => (
+        {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             innerRef={provided.innerRef}
+            isDragging={snapshot.isDragging}
           >
             {this.props.task.content}
           </Container>
