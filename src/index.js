e76b97252347164a6df93529f6d4d98f217451a6
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 19:57:40 +1000

    onDragStart and onDragUpdate

--- src/index.js
+++ src/index.js
@@ -9,7 +9,22 @@ import Column from "./Column";
 class App extends React.Component {
   state = initialData;
 
+  onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153,141,217,${opacity}`;
+  };
+
   onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { destination, source, draggableId } = result;
 
     if (!destination) return;
@@ -36,7 +51,11 @@ class App extends React.Component {
   };
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
         {this.state.columnOder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(id => this.state.tasks[id]);
