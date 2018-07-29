b9ceb2318e68581a6051c77addda1516982cecc6
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 22:51:57 +1000

    vertical display tasks

--- src/Column.js
+++ src/Column.js
@@ -7,7 +7,6 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  width: 220px;
   display: flex;
   flex-direction: column;
 `;
@@ -18,8 +17,8 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${props => (props.isDragOver ? "skyblue" : "white")};
-  flex-grow: 1;
   min-height: 100px;
+  display: flex;
 `;
 
 export default class Column extends React.Component {
--- src/Task.js
+++ src/Task.js
@@ -4,45 +4,32 @@ import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
-  border-radius; 2px;
+  border-radius: 50%;
   background: white;
   padding: 8px;
   margin-bottom: 8px;
   display: flex;
-  background-color:${props =>
-    props.isDragDisabled
-      ? "lightgrey"
-      : props.isDragging
-        ? "lightgreen"
-        : "white"}
-`;
-
-const Handle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: orange;
-  border-radius: 4px;
-  margin-right: 8px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => (props.isDragging ? "lightgreen" : "white")};
 `;
 
 export default class Task extends React.Component {
   render() {
     const isDragDisabled = this.props.task.id === "task-1";
     return (
-      <Draggable
-        draggableId={this.props.task.id}
-        isDragDisabled={isDragDisabled}
-        index={this.props.index}
-      >
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
         {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
+            {...provided.dragHandleProps}
             innerRef={provided.innerRef}
             isDragging={snapshot.isDragging}
-            isDragDisabled={isDragDisabled}
           >
-            <Handle {...provided.dragHandleProps} />
-            {this.props.task.content}
+            {this.props.task.content[0]}
           </Container>
         )}
       </Draggable>
