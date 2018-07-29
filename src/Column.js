054aef94aea574a20a9fac67b8a8bea03a67e1d1
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 22:41:54 +1000

    use type to disable drop area

--- src/Column.js
+++ src/Column.js
@@ -27,7 +27,10 @@ export default class Column extends React.Component {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <Droppable droppableId={this.props.column.id}>
+        <Droppable
+          droppableId={this.props.column.id}
+          type={this.props.column.id === "column-3" ? "done" : "active"}
+        >
           {(provided, snapshot) => (
             <TaskList
               innerRef={provided.innerRef}
