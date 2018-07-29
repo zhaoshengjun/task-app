7b6a94bca5c17cb6f1adc4713d5d4a94d1bcf0ba
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-29 20:32:15 +1000

    use drag handle

--- src/Task.js
+++ src/Task.js
@@ -8,7 +8,16 @@ const Container = styled.div`
   background: white;
   padding: 8px;
   margin-bottom: 8px;
+  display: flex;
   background-color:${props => (props.isDragging ? "lightgreen" : "white")}
+`;
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `;
 
 export default class Task extends React.Component {
@@ -18,10 +27,10 @@ export default class Task extends React.Component {
         {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
-            {...provided.dragHandleProps}
             innerRef={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
+            <Handle {...provided.dragHandleProps} />
             {this.props.task.content}
           </Container>
         )}
